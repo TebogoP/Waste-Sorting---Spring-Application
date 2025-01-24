5e9570b5545ee573f8ed8bf2803aeb3325a328bd
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-01-25 00:34:41 +0200

    Added getters and setters in WasteCategory class

--- com.enviro.assessment.grad001.TebogoPhiri/com.enviro.assessment.grad001.TebogoPhiri/src/main/java/com/enviro/assessment/grad001/TebogoPhiri/model/WasteCategory.java
+++ com.enviro.assessment.grad001.TebogoPhiri/com.enviro.assessment.grad001.TebogoPhiri/src/main/java/com/enviro/assessment/grad001/TebogoPhiri/model/WasteCategory.java
@@ -1,5 +1,44 @@
 package com.enviro.assessment.grad001.TebogoPhiri.model;
 
 public class WasteCategory  {
+    public WasteCategory() {
+    }
+    public WasteCategory(int id, String name, String description) {
+        setId(id);
+        setName(name);
+        setDescription(description);
+    }
+    private int id;
 
+    private String name;
+
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"id\": " + id + ", " +
+                "\"name\": \"" + name + "\", " +
+                "\"description\": \"" + description + "\"" +
+                "}";
+    }
 }
