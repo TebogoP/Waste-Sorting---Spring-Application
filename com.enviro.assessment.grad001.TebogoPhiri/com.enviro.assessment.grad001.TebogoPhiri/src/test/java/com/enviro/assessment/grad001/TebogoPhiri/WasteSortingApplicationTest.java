1b39669502e0786dd7414477f760032a381a4951
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-01-24 23:06:51 +0200

    Implementation of TDD with First test

---
+++ com.enviro.assessment.grad001.TebogoPhiri/com.enviro.assessment.grad001.TebogoPhiri/src/test/java/com/enviro/assessment/grad001/TebogoPhiri/WasteSortingApplicationTest.java
@@ -0,0 +1,46 @@
+package com.enviro.assessment.grad001.TebogoPhiri;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class WasteSortingApplicationTest {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private PrintStream originalOut;
+
+    @BeforeEach
+    void setUp() {
+        // Redirect System.out to capture print stream
+        originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void tearDown() {
+        // Restore original output stream
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void testApplicationStartup(){
+        // Arrange
+        /* Simulate any necessary arguments for the main method*/
+        //String[] args = {};
+
+        WasteSortingApplication.main(new String[]{});
+
+        // Asserts
+        String actualOutput = outputStream.toString().trim();
+        assertNotNull(actualOutput, "Output should not be null");
+        assertFalse(actualOutput.isEmpty(), "Output should not be empty");
+        assertTrue(actualOutput.contains("Waste Sorting Application started successfully"));
+    }
+
+
+}
