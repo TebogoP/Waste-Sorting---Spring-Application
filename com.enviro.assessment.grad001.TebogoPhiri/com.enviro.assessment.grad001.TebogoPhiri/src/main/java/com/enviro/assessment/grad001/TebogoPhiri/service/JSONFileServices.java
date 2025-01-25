ab922f790cee471680fe5442afcc210ef2d72f80
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-01-25 21:53:01 +0200

    Added a generic saveToFile and readFromFile methods. Makes this file reusable

--- com.enviro.assessment.grad001.TebogoPhiri/com.enviro.assessment.grad001.TebogoPhiri/src/main/java/com/enviro/assessment/grad001/TebogoPhiri/service/JSONFileServices.java
+++ com.enviro.assessment.grad001.TebogoPhiri/com.enviro.assessment.grad001.TebogoPhiri/src/main/java/com/enviro/assessment/grad001/TebogoPhiri/service/JSONFileServices.java
@@ -13,5 +13,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class JSONFileServices<T> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * Save the list of WasteCategory objects to the JSON file.
+     * @param items The list of WasteCategory objects to save.
+     * @throws IOException If writing to the JSON file fails.
+     */
+    public void saveToFile(List<T> items, String filePath) throws IOException {
+        if (items == null) throw new NullPointerException("We can't save null objects");
+        objectMapper.writeValue(new File(filePath), items);
+    }
+    /**
+     * Read the list of WasteCategory objects from the JSON file.
+     * @return The list of WasteCategory objects.
+     * @throws IOException If reading from the JSON file fails.
+     */
+    public List<T> readFromFile(String filePath, Class<T> clazz) throws IOException {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            return new ArrayList<>();
+        }
+        CollectionType listType = objectMapper.getTypeFactory()
+                .constructCollectionType(ArrayList.class, clazz);
+        return objectMapper.readValue(file, listType);
+    }
 }
