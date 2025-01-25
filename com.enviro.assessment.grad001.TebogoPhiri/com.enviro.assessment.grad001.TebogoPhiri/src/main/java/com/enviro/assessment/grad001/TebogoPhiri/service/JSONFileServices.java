6c6fcf7f851c51563dc9fe0120cebb364cca3b48
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-01-25 09:40:44 +0200

    Added Services for writing and reading to a json file. Going to use that a json file as temporary database

---
+++ com.enviro.assessment.grad001.TebogoPhiri/com.enviro.assessment.grad001.TebogoPhiri/src/main/java/com/enviro/assessment/grad001/TebogoPhiri/service/JSONFileServices.java
@@ -0,0 +1,17 @@
+package com.enviro.assessment.grad001.TebogoPhiri.service;
+
+import com.enviro.assessment.grad001.TebogoPhiri.model.WasteCategory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.type.CollectionType;
+import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class JSONFileServices<T> {
+
+}
