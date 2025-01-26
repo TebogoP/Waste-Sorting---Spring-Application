776fc10b4bfa62fddec80660cda19e6c0eee400d
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-01-26 23:03:24 +0200

    Added Database schema (how data will be organized within the relational database)

---
+++ com.enviro.assessment.grad001.TebogoPhiri/com.enviro.assessment.grad001.TebogoPhiri/src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+-- Create table for WasteCategory
+CREATE TABLE IF NOT EXISTS WasteCategory (
+    id INT NOT NULL PRIMARY KEY,        -- Primary key,
+    name VARCHAR(255) NOT NULL,         -- Waste category name
+    description TEXT NOT NULL           -- Description of the waste category
+
+);
+
+-- Create table for RecyclingTip
+CREATE TABLE IF NOT EXISTS RecyclingTip (
+    id INT NOT NULL PRIMARY KEY,        -- Primary key,
+    category VARCHAR(255) NOT NULL,     -- Category of the recycling tip
+    tip TEXT NOT NULL                   -- Recycling tip description
+    --wasteCategoryId INT,              -- Foreign key referencing WasteCategory
+    --CONSTRAINT fk_recycling_wasteCategory FOREIGN KEY (wasteCategoryId) REFERENCES WasteCategory(id)
+);
+
+-- Create table for DisposalGuideline
+CREATE TABLE IF NOT EXISTS DisposalGuideline (
+    id INT NOT NULL PRIMARY KEY,        -- Primary key,
+    wasteType VARCHAR(255) NOT NULL,    -- Type of waste for which the guideline applies
+    guideline TEXT NOT NULL             -- Disposal guideline description
+    --wasteCategoryId INT,              -- Foreign key referencing WasteCategory
+    --CONSTRAINT fk_disposal_wasteCategory FOREIGN KEY (wasteCategoryId) REFERENCES WasteCategory(id)
+);
