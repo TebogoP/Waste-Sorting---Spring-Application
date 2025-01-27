daab2e97ed10114536b6e06171f45d663f4e285c
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-01-27 12:58:09 +0200

    Forgot the WasteCategories record for the JsonDataLoader class.

---
+++ com.enviro.assessment.grad001.TebogoPhiri/com.enviro.assessment.grad001.TebogoPhiri/src/main/java/com/enviro/assessment/grad001/TebogoPhiri/WasteCategories/model/WasteCategories.java
@@ -0,0 +1,18 @@
+package com.enviro.assessment.grad001.TebogoPhiri.WasteCategories.model;
+
+import java.util.List;
+public record WasteCategories(List<WasteCategory> waste) {
+    /*Record is immuntable(unchangable)
+     * Record has its own toString, hasCode, equals, getters (no setters)
+     * Record Automatically generates a constructor
+     * Record used for modeling data-centric objects (data containers)
+     * Record fields are final and cannot be reassigned once initialized.
+     * Record encourages immutability for thread safety and cleaner design.
+     * Record cannot extend other classes (i.e., they are final by design).
+     * Record can implement interfaces, which allows for some degree of polymorphism.
+     * Record performance is similar to classes.
+     * Record due to immutability and auto-generated equals()/hashCode(), they might be slightly more efficient in contexts like hash-based collections.
+     * Use records when you want a data-centric, immutable structure with minimal boilerplate.
+     * Use classes when you need full flexibility, complex behavior, or mutability.
+     * */
+}
