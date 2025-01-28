4647098cb5eef156d87d19a9e290b9cd18349aba
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-01-28 16:28:42 +0200

    Introduced Interfaces to use ListCrudRepository for controller

--- com.enviro.assessment.grad001.TebogoPhiri/src/main/java/com/enviro/assessment/grad001/TebogoPhiri/WasteCategories/service/WasteCatRepository.java
+++ com.enviro.assessment.grad001.TebogoPhiri/src/main/java/com/enviro/assessment/grad001/TebogoPhiri/WasteCategories/service/WasteCatRepository.java
@@ -3,8 +3,9 @@ package com.enviro.assessment.grad001.TebogoPhiri.WasteCategories.service;
 import com.enviro.assessment.grad001.TebogoPhiri.WasteCategories.model.WasteCategory;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface WasteCatRepository  extends ListCrudRepository<WasteCategory, Integer> {
 
 }
